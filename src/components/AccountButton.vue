e4cd7c099fb861dc8a959b4b88e24190d542d8ba
Carjardying <carolinejardy.pro@gmail.com>
2025-07-02 17:16:08 +0200

    vue home and accountButton to access login page

---
+++ src/components/AccountButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
+
+<template>
+  <button @click="open = true"><img src="/src/assets/images/icons/account_light_account.png" alt="account link" class="account max-h-[30px]"></button>
+
+  <div v-if="open" class="modal flex items-center flex-col size-24 m-[20px]">
+    <a href="http://localhost:5500/login" class="bg-medium-brown-700"> Login </a>
+    <a href="http://localhost:5500/register" class="bg-light-brown-600"> Sign up </a> 
+    <button @click="open = false">Close</button>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 13%;
+  /* width: 150px; */
+  /* left: 50%;
+  margin-left: -150px; */
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,46 @@
 <!-- Page à modifier par la suite -->
 
+
 <template>
-  <h2>Bienvenue sur la page d'accueil</h2>
+
+  <div class="bg-dark-aubergine-800 h-dvh">
+
+    <header class="bg-dark-aubergine-800 text-medium-beige-300">
+
+      <div class="flex items-center justify-between">
+
+        <img src="/src/assets/images/logos/logo_light.png" alt="logo" class="logo max-h-[60px] m-[1rem]"></img> 
+        <a href="http://localhost:5500/catalogue">BROWSE FURNITURES</a>
+  
+        <div class="flex justify-around m-[1rem]">
+          <!-- -->
+          <AccountButton></AccountButton>
+          <a href="http://localhost:5500/cart"> <img src="/src/assets/images/icons/cart_light_icon.png" alt="cart link" class="cart max-h-[30px]"></a>
+
+        </div>
+
+      </div>
+
+    </header> <!-- component Header.vue -->
+  
+    <div class="bg-dark-aubergine-800 h-dvh">
+      <img src="/src/assets/images/interior/homepage_picture.PNG" alt="Homepage image" class="imageHome">
+
+      <div class="text-medium-beige-300 flex items-center justify-between">
+        <h1 class="font-heading text-6xl m-[2rem]">Interior Stories</h1> 
+        <p class="font-body m-[2rem]">We curate statement pieces that turn living <br/> spaces into narratives of your life.</p>
+      </div>
+    </div>
+  
+    <footer class="bg-khaki-900 text-medium-beige-300 flex items-center justify-between">
+      <img src="/src/assets/images/logos/logo_light.png" alt="logo" class="logo max-h-[60px] m-[1rem]"></img> 
+      <p class="font-body m-[1rem]">©Copyright 2025 Interior Stories</p>
+    </footer> <!-- component Footer.vue -->
+
+  </div>
+
 </template>
+
+<script setup>
+  import AccountButton from '../components/AccountButton.vue';
+</script>
