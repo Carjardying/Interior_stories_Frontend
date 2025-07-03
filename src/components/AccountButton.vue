29199cdbb76df3861648454d363924465988b555
Carjardying <carolinejardy.pro@gmail.com>
2025-07-03 09:41:52 +0200

    commented for passation or continue later

--- src/components/AccountButton.vue
+++ src/components/AccountButton.vue
@@ -1,22 +1,29 @@
-<script setup>
-import { ref } from 'vue'
-
-const open = ref(false)
-</script>
-
 <template>
   <button @click="open = true"><img src="/src/assets/images/icons/account_light_account.png" alt="account link" class="account max-h-[30px]"></button>
 
   <div v-if="open" class="modal flex items-center flex-col size-24 m-[20px]">
-    <RouterLink to="/login" class="bg-medium-brown-700">Login</RouterLink>
-    <RouterLink to="/register" class="bg-light-brown-600">Sign up</RouterLink>  
-    <button @click="open = false">Close</button> <!--à transformer en "on click à coté de la modale => on ferme"-->
+    <RouterLink to="/login" class="bg-medium-brown-700">Login</RouterLink> <!--Make them rectangular with a little margin around them and text in center-->
+    <RouterLink to="/register" class="bg-light-brown-600">Sign up</RouterLink> 
+    <button @click="open = false">Close</button> <!--change in js into "if click out of the modal{ open = false}"-->
   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+/** TO DO : CHANGE MODALE:
+ * const connected = ref(false)
+ *  IF (connected == true){
+ *    RouterLink to="/login" => RouterLink to="/account"
+ *    RouterLink register =>  button @click= "disconnect"
+ *
+ * } */
+</script>
+
 <style scoped>
 .modal {
-  position: fixed;
+  position: absolute;
   z-index: 999;
   top: 13%;
   /* width: 150px; */
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,22 +7,21 @@
 
     <header class="bg-dark-aubergine-800 text-medium-beige-300">
 
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between"> <!-- I see no diff between justify-around and justify-between as it should show a diff-->
 
         <img src="/src/assets/images/logos/logo_light.png" alt="logo" class="logo max-h-[60px] m-[1rem]"></img>
         <RouterLink to="/catalogue" class="text-medium-beige-300">BROWSE FURNITURES</RouterLink>  
 
   
-        <div class="flex justify-around m-[1rem]">
-          <!-- -->
-          <AccountButton></AccountButton>
-          <RouterLink to="/cart" class="text-medium-beige-300"><img src="/src/assets/images/icons/cart_light_icon.png" alt="cart link" class="cart max-h-[30px]"></RouterLink>
+        <div class="flex justify-between border border-medium-beige-300"> <!-- I see no diff between justify-around/between changing rem don't do anything -->
+          <AccountButton class="text-medium-beige-300 border border-medium-beige-300 p-[1rem]"></AccountButton>
+          <RouterLink to="/cart" class="text-medium-beige-300 border border-medium-beige-300 p-[1rem]"><img src="/src/assets/images/icons/cart_light_icon.png" alt="cart link" class="cart max-h-[30px]"></RouterLink>
 
         </div>
 
       </div>
 
-    </header> <!-- component Header.vue -->
+    </header> <!-- component Header.vue-->
   
     <div class="bg-dark-aubergine-800 h-dvh">
       <img src="/src/assets/images/interior/homepage_picture.PNG" alt="Homepage image" class="imageHome">
