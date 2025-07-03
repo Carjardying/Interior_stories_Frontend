0d36fcdae0e922fb7b71ed54fd743a1ed1f1b9e0
Carjardying <carolinejardy.pro@gmail.com>
2025-07-03 15:13:22 +0200

    header, fooer title done, cards started

--- src/pages/Catalogue.vue
+++ src/pages/Catalogue.vue
@@ -1,3 +1,47 @@
 <template>
-    
+  <div class="bg-light-beige-100 h-dvh">
+    <Header></Header>
+    <div>
+      <h1 class="font-heading text-6xl m-[2rem] text-dark-aubergine-800">
+        Furnitures
+      </h1>
+    </div>
+    <div>
+      Furniture's Carts here
+      <div>
+        <div class="outline-solid m-8">
+          <div>
+            <img
+              src="/src/assets/images/furnitures/soriana_sofa.PNG"
+              alt="sofa soriana"
+            />
+          </div>
+          <div class="outline-solid h-1/3">Name</div>
+          <div class="outline-solid">Price</div>
+          <div class="outline-solid">
+            <button>+ Add to cart</button>
+          </div>
+        </div>
+        <div class="outline-solid m-8">
+          <div>
+            <img
+              src="/src/assets/images/furnitures/cassina_lc3_sofa.PNG"
+              alt="sofa Cassina"
+            />
+          </div>
+          <div class="outline-solid h-1/3">Name</div>
+          <div class="outline-solid">Price</div>
+          <div class="outline-solid">
+            <button>+ Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
+
+<script setup>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+</script>
