cf72d77e4b0ad3b8fd565bca2b2cfb022015c051
Carjardying <carolinejardy.pro@gmail.com>
2025-07-03 17:54:29 +0200

    added 4 divs to see differences and errors in sizing

--- src/pages/Catalogue.vue
+++ src/pages/Catalogue.vue
@@ -1,40 +1,73 @@
 <template>
-  <div class="bg-light-beige-100 h-dvh">
+  <div class="bg-light-beige-100 min-h-100">
     <Header></Header>
     <div>
-      <h1 class="font-heading text-6xl m-[2rem] text-dark-aubergine-800">
+      <h1 class="font-heading text-6xl !m-[2rem] text-dark-aubergine-800">
         Furnitures
       </h1>
     </div>
-    <div>
-      Furniture's Carts here
-      <div>
-        <div class="outline-solid m-8">
+    <div class="!m-4">
+      Furniture's Carts here. Will be replaced by cart.vue
+
+      <div class="min-h-100 grid grid-cols-2">
+         
+        <div class="col-span-1 outline-solid !m-8">
           <div>
             <img
               src="/src/assets/images/furnitures/soriana_sofa.PNG"
               alt="sofa soriana"
             />
           </div>
-          <div class="outline-solid h-1/3">Name</div>
-          <div class="outline-solid">Price</div>
-          <div class="outline-solid">
-            <button>+ Add to cart</button>
+          <div class="font-body text-dark-aubergine-800">Sofa Soriana</div>
+          <div class="font-body text-dark-aubergine-800">EUR 450,00</div>
+          <div class="font-body bg-dark-aubergine-800 text-medium-beige-300">
+            <RouterLink to="/cart">+ Add to cart</RouterLink>
           </div>
         </div>
-        <div class="outline-solid m-8">
+
+        <div class="col-span-1 outline-solid !m-8">
           <div>
             <img
               src="/src/assets/images/furnitures/cassina_lc3_sofa.PNG"
               alt="sofa Cassina"
+              class="h-full object-cover"
             />
           </div>
-          <div class="outline-solid h-1/3">Name</div>
-          <div class="outline-solid">Price</div>
-          <div class="outline-solid">
-            <button>+ Add to cart</button>
+          <div class="font-body text-dark-aubergine-800">Sofa Cassina</div>
+          <div class="font-body text-dark-aubergine-800">EUR 350,00</div>
+          <div class="font-body bg-dark-aubergine-800 text-medium-beige-300">
+            <RouterLink to="/cart">+ Add to cart</RouterLink>
           </div>
         </div>
+
+        <div class="col-span-1 outline-solid !m-8">
+          <div>
+            <img
+              src="/src/assets/images/furnitures/togo_sofa.PNG"
+              alt="sofa Togo"
+            />
+          </div>
+          <div class="font-body text-dark-aubergine-800">Sofa Togo</div>
+          <div class="font-body text-dark-aubergine-800">EUR 450,00</div>
+          <div class="font-body bg-dark-aubergine-800 text-medium-beige-300">
+            <RouterLink to="/cart">+ Add to cart</RouterLink>
+          </div>
+        </div>
+
+        <div class="col-span-1 outline-solid !m-8">
+          <div>
+            <img
+              src="/src/assets/images/furnitures/eames_lounge_chair.PNG"
+              alt="Eames Lounge Chair and Ottoman"
+            />
+          </div>
+          <div class="font-body text-dark-aubergine-800">Eames Lounge Chair and Ottoman</div>
+          <div class="font-body text-dark-aubergine-800">EUR 4000,00</div>
+          <div class="font-body bg-dark-aubergine-800 text-medium-beige-300">
+            <RouterLink to="/cart">+ Add to cart</RouterLink>
+          </div>
+        </div>
+
       </div>
     </div>
     <Footer></Footer>
