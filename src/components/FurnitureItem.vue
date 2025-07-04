7333cbff8bb2abe03f83f6028742c48d23348b4d
chazera <hazera.c@gmail.com>
2025-07-04 14:44:06 +0200

    update: catalogue and furnitureItem

--- src/components/FurnitureItem.vue
+++ src/components/FurnitureItem.vue
@@ -1,20 +1,15 @@
 <!-- components/FurnitureCard.vue -->
 <template>
 
-  <form @submit.prevent="" class="col-span-1  m-8">
-          <div class="h-150 overflow-hidden">
-            <img
-              src="/src/assets/images/furnitures/cassina_lc3_sofa.PNG"
-              alt="sofa Cassina"
-              class="w-full h-full object-cover"
-            />
-          </div>
-          <h2 class="font-body text-dark-aubergine-800 p-1">Sofa Cassina</h2>
-          <p class="font-body text-dark-aubergine-800 p-1">EUR 350,00</p>
-          <button type=""submit class=" w-full font-body bg-dark-aubergine-800 text-medium-beige-300 p-2">
-             + Add to Cart
-            </button>
-           
+  <form @submit.prevent="addToCart" class="col-span-1  m-8">
+    <div class="h-150 overflow-hidden">
+      <img :src="image" :alt="name" class="w-full h-full object-cover" />
+    </div>
+    <h2 class="font-body text-dark-aubergine-800 p-1">{{ name }}</h2>
+    <p class="font-body text-dark-aubergine-800 p-1">EUR {{ price }}</p>
+    <button type="submit" class=" w-full font-body bg-dark-aubergine-800 text-medium-beige-300 p-2">
+      + Add to Cart
+    </button>
   </form>
 </template>
 
@@ -24,4 +19,8 @@ defineProps({
   price: String,
   image: String,
 });
+
+function addToCart() {
+  // faire l'ajout du meuble dans une commande
+}
 </script>
--- src/pages/Catalogue.vue
+++ src/pages/Catalogue.vue
@@ -2,64 +2,12 @@
   <div class="bg-light-beige-100 min-h-100">
     <Header></Header>
     <div>
-      <h1 class="font-heading text-6xl  m-[2rem] text-dark-aubergine-800">
-        Furnitures
-      </h1>
+      <h1 class="font-heading text-6xl  m-[2rem] text-dark-aubergine-800">Furnitures</h1>
     </div>
     <div class=" m-4">
-      Furniture's Carts here. Will be replaced by cart.vue
-
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-         
-        <div class="col-span-1  m-8">
-          <div class="h-150 overflow-hidden">
-            <img
-              src="/src/assets/images/furnitures/soriana_sofa.PNG"
-              alt="sofa soriana"
-              class="w-full h-full object-cover"
-            />
-          </div>
-          <div class="font-body text-dark-aubergine-800">Sofa Soriana</div>
-          <div class="font-body text-dark-aubergine-800">EUR 450,00</div>
-          <div class="font-body bg-dark-aubergine-800 text-medium-beige-300">
-            <RouterLink to="/cart">+ Add to cart</RouterLink>
-          </div>
-        </div>
-
-        <FurnitureItem></FurnitureItem>
-        
-        <div class="col-span-1  m-8">
-          <div class="h-150 overflow-hidden">
-            <img
-              src="/src/assets/images/furnitures/togo_sofa.PNG"
-              alt="sofa Togo"
-              class="w-full h-full object-cover"
-  
-            />
-          </div>
-          <div class="font-body text-dark-aubergine-800">Sofa Togo</div>
-          <div class="font-body text-dark-aubergine-800">EUR 450,00</div>
-          <div class="font-body bg-dark-aubergine-800 text-medium-beige-300">
-            <RouterLink to="/cart">+ Add to cart</RouterLink>
-          </div>
-        </div>
-
-        <div class="col-span-1  m-8">
-          <div class="h-150 overflow-hidden">
-            <img
-              src="/src/assets/images/furnitures/eames_lounge_chair.PNG"
-              alt="Eames Lounge Chair and Ottoman"
-              class="w-full h-full object-cover"
-
-            />
-          </div>
-          <div class="font-body text-dark-aubergine-800">Eames Lounge Chair and Ottoman</div>
-          <div class="font-body text-dark-aubergine-800">EUR 4000,00</div>
-          <div class="font-body bg-dark-aubergine-800 text-medium-beige-300">
-            <RouterLink to="/cart">+ Add to cart</RouterLink>
-          </div>
-        </div>
-
+        <FurnitureItem v-for="furniture in furnitureList" :key="furniture.id" :name="furniture.name"
+          :price="furniture.price" :image="furniture.image" />
       </div>
     </div>
     <Footer></Footer>
@@ -69,5 +17,21 @@
 <script setup>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
-import FurnitureItem from "../components/FurnitureItem.vue";
+import FurnitureItem from '../components/FurnitureItem.vue';
+import { ref, onMounted } from 'vue';
+
+const furnitureList = ref([]);
+
+onMounted(() => {
+  fetch("http://localhost:8000/api/furnitures", {
+    method: 'Get',
+  })
+    .then(response => response.json())
+    .then(data => {
+      furnitureList.value = data;
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
 </script>
