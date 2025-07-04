b88f2bb12bf6f0b3e6f751e7499770189ae96f9e
Carjardying <carolinejardy.pro@gmail.com>
2025-07-04 16:30:00 +0200

    Update: finishing Home.vue with new visuals

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,21 +3,20 @@
 
     <!-- Refaire un component header qui varie light/dark, si possible -->
     <header class="bg-dark-aubergine-800 text-medium-beige-300">
-      <div class="flex items-center justify-between">
-        <!-- I see no diff between justify-around and justify-between as it should show a diff-->
-        <img src="/src/assets/images/logos/logo_light.png" alt="Brand logo light version"
-          class="logo max-h-[60px] m-[1rem]"></img>
-        <RouterLink to="/Catalogue" class="text-medium-beige-300">
-          Go to our Catalogue
-        </RouterLink>
-        <div class="flex justify-between ">
-          <!-- I see no diff between justify-around/between changing rem don't do anything -->
-          <!-- Account button is not completely recognized by home.vue as a button so the cursor doesn't change-->
 
-          <!-- Component a changer pour variable en light/dark, pour l'instant deux component ça fonctionne-->
+      <div class="flex items-center justify-between">
+        
+        <div class="flex items-center ">
+          <img src="/src/assets/images/logos/logo_light.png" alt="Brand logo light version"
+            class="logo h-20 m-[1rem]"></img>
+          <h1 class="text-medium-beige-300 ">Interior Stories</h1>
+        </div>
+
+        <div class="flex item-center space-x-6 pr-10">
           <AccountButtonHome></AccountButtonHome>
           <CartButtonHome></CartButtonHome>
         </div>
+
       </div>
     </header>
 
@@ -25,9 +24,11 @@
       <img src="/src/assets/images/interior/homepage_picture.PNG" alt="Living room with modern interior"
         class="imageHome" />
       <div class="text-medium-beige-300 flex items-center justify-between">
-        <h1 class="font-heading text-6xl p-[2rem]">Interior Stories</h1>
+        <RouterLink to="/Catalogue" class="font-heading text-6xl p-[2rem]">
+          Explore our selection
+        </RouterLink>
         <p class="font-body p-[2rem]">
-          We curate statement pieces that turn living <br />
+          We curate statement pieces that turn living <br>
           spaces into narratives of your life.
         </p>
       </div>
