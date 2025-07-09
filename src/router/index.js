d71fea2535719196f5180c3abc165434c1dd468a
chazera <hazera.c@gmail.com>
2025-07-09 17:20:25 +0200

    fix: router without page undone, ready for demoday

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LogIn from '../pages/Login.vue';
 import Home from '../pages/Home.vue';
-import Admin from '../pages/Admin.vue';
-import Account from '../pages/Account.vue';
+// import Admin from '../pages/Admin.vue';
+// import Account from '../pages/Account.vue';
 import Cart from '../pages/Cart.vue';
 import Catalogue from '../pages/Catalogue.vue';
-import Register from '../pages/Register.vue';
+// import Register from '../pages/Register.vue';
 import FurnitureDetails from '../pages/FurnitureDetails.vue';
 
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/login', component: LogIn },
   { path: '/home', component: Home },
-  { path: '/admin', component: Admin },
-  { path: '/account', component: Account },
+  // { path: '/admin', component: Admin },
+  // { path: '/account', component: Account },
   { path: '/cart', component: Cart },
   { path: '/catalogue', component: Catalogue },
-  { path: '/register', component: Register },
+  // { path: '/register', component: Register },
   { path: '/furniture-details/:id', name: "furniture-details", component: FurnitureDetails },
 ];
 
